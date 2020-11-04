a9b26ea742dd1870cc41ae4b3ff52c295851bb14
Sean Hung <35648173+seanhung07@users.noreply.github.com>
2020-11-03 22:55:44 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app" class="container">
+    <img alt="Vue logo" src="./assets/logo.jpg" class="logo">
+    <Todo></Todo>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Todo
+  }
+}
+</script>
+
+<style lang="scss">
+*{
+  box-sizing: border-box;
+}
+.container{
+  max-width: 600px;
+  margin: 0 auto;
+}
+.logo{
+  display: block;
+  margin: 30px auto;
+  height: 105px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,245 @@
+<template>
+  <div>
+    <input
+      type="text"
+      class="todo-input"
+      placeholder="Add to the list"
+      v-model="newTodo"
+      @keyup.enter="addtodo"
+    />
+    <transition-group name="fade">
+      <div
+        v-for="(todo, index) in todosfilter"
+        :key="todo.id"
+        class="todo-list"
+      >
+        <div class="todo-list-left">
+          <input type="checkbox" v-model="todo.completed" />
+          <div
+            v-if="!todo.edit"
+            @dblclick="edittodo(todo)"
+            class="todo-list-label"
+            :class="{ completed: todo.completed }"
+          >
+            {{ todo.title }}
+          </div>
+          <input
+            v-else
+            type="text"
+            v-model="todo.title"
+            class="todo-list-edit"
+            @blur="donetodo(todo)"
+            @keyup.enter="donetodo(todo)"
+            @keyup.esc="canceledit(todo)"
+            v-focus
+          />
+        </div>
+        <div class="remove-item" @click="removetodo(index)">
+          &times;
+        </div>
+      </div>
+    </transition-group>
+    <div class="extra-container">
+      <div>
+        <label>
+          <input type="checkbox" :checked="!anyremaining" @change="checkall" />
+          Check All
+        </label>
+      </div>
+      <div>{{ remaining }} items left</div>
+    </div>
+    <div class="extra-container">
+      <div>
+        <button
+          class="btn btn-secondary mr-1"
+          :class="{ active: filter == 'all' }"
+          @click="filter = 'all'"
+        >
+          ALL
+        </button>
+        <button
+          class="btn btn-secondary mr-1"
+          :class="{ active: filter == 'active' }"
+          @click="filter = 'active'"
+        >
+          Active
+        </button>
+        <button
+          class="btn btn-secondary mr-1"
+          :class="{ active: filter == 'completed' }"
+          @click="filter = 'completed'"
+        >
+          Completed
+        </button>
+      </div>
+      <div>
+        <button
+          v-if="showclearall"
+          class="btn btn-success"
+          @click="clearcompleted"
+        >
+          Clear Completed
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  data() {
+    return {
+      newTodo: "",
+      idfortodo: 3,
+      beforeEditCache: "",
+      filter: "all",
+      todos: [
+        {
+          id: 1,
+          title: "Default Item (Delete first)",
+          completed: false,
+          edit: false,
+        },
+      ],
+    };
+  },
+  directives: {
+    focus: {
+      // directive definition
+      inserted: function(el) {
+        el.focus();
+      },
+    },
+  },
+  computed: {
+    remaining() {
+      return this.todos.filter((todo) => !todo.completed).length;
+    },
+    anyremaining() {
+      return this.remaining != 0;
+    },
+    todosfilter() {
+      if (this.filter == "all") {
+        return this.todos;
+      } else if (this.filter == "active") {
+        return this.todos.filter((todo) => !todo.completed);
+      } else if (this.filter == "completed") {
+        return this.todos.filter((todo) => todo.completed);
+      }
+      return this.todos;
+    },
+    showclearall() {
+      return this.todos.filter((todo) => todo.completed).length > 0;
+    },
+  },
+  methods: {
+    addtodo() {
+      if (this.newTodo.trim() == 0) {
+        return;
+      }
+      this.todos.push({
+        id: this.idfortodo,
+        title: this.newTodo,
+        completed: false,
+      });
+      (this.newTodo = ""), this.idfortodo++;
+    },
+    removetodo(index) {
+      this.todos.splice(index, 1);
+    },
+    edittodo(todo) {
+      this.beforeEditCache = todo.title;
+      todo.edit = true;
+    },
+    donetodo(todo) {
+      if (todo.title.trim() == "") {
+        todo.title = this.todo.beforeEditCache;
+      }
+      todo.edit = false;
+    },
+    canceledit(todo) {
+      todo.title = this.beforeEditCache;
+      todo.edit = false;
+    },
+    checkall() {
+      this.todos.forEach((todo) => (todo.completed = event.target.checked));
+    },
+    clearcompleted() {
+      this.todos = this.todos.filter((todo) => !todo.completed);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.todo-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
+  &:focus {
+    outline: 0;
+  }
+}
+.todo-list {
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.remove-item {
+  cursor: pointer;
+  font-size: 30px;
+  margin-left: 14px;
+  &:hover {
+    color: red;
+  }
+}
+.todo-list-left {
+  display: flex;
+  align-items: center;
+}
+.todo-list-label {
+  padding: 10px;
+  border: 1 px solid white;
+  margin-left: 12px;
+}
+.todo-list-edit {
+  font-size: 24px;
+  color: #2c3e50;
+  margin-left: 12px;
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #cccccc;
+  &:focus {
+    outline: none;
+  }
+}
+.completed {
+  text-decoration: line-through;
+  color: red;
+}
+.extra-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  border-top: 1px solid lightgrey;
+  padding-top: 16px;
+  margin-bottom: 14px;
+}
+
+.fade-enter-active {
+  transition: all 0.3s ease;
+}
+.fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.fade-enter, .fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
